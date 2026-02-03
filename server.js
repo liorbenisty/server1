6a2d09a8d9032adaa5d7fd55150ddd0bdf6f8ce2
lior <benistyli@mta.ac.il>
2026-02-03 16:58:42 +0200

    env update

--- server.js
+++ server.js
@@ -8,10 +8,32 @@ app.use(cors());
 app.use(express.json());
 
 // Google Sheets setup
-const auth = new google.auth.GoogleAuth({
-    keyFile: 'credentials.json',
-    scopes: ['https://www.googleapis.com/auth/spreadsheets'],
-});
+let auth;
+if (process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL) {
+    // Use environment variables (production)
+    auth = new google.auth.GoogleAuth({
+        credentials: {
+            type: 'service_account',
+            project_id: process.env.GOOGLE_PROJECT_ID,
+            private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+            private_key: process.env.GOOGLE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+            client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
+            client_id: process.env.GOOGLE_CLIENT_ID,
+            auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+            token_uri: 'https://oauth2.googleapis.com/token',
+            auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+            client_x509_cert_url: `https://www.googleapis.com/robot/v1/metadata/x509/${encodeURIComponent(process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL)}`,
+            universe_domain: 'googleapis.com'
+        },
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+} else {
+    // Use credentials file (development)
+    auth = new google.auth.GoogleAuth({
+        keyFile: 'credentials.json',
+        scopes: ['https://www.googleapis.com/auth/spreadsheets'],
+    });
+}
 
 const sheets = google.sheets({ version: 'v4', auth });
 
